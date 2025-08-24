43a8a2151b9abc29015c861a7be0d734da08b243
ajanipriyanka14 <ajanipriyanka13@gmail.com>
2025-08-24 08:49:49 -0700

    avrage.cpp

---
+++ avrage.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+	 float n1,n2,n3,n4,avg;
+	 clrscr();
+	 cout<<"enter four number";
+	 cin>>n1>>n2>>n3>>n4;
+	 avg=(n1+n2+n3+n4)/4;
+	 cout<<"avrage"<<avg;
+	getch();
+	return 0;
+}
