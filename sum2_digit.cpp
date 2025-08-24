cadc4012bcdd2ee32b617c62a5bffccd44ceab96
ajanipriyanka14 <ajanipriyanka13@gmail.com>
2025-08-24 08:47:17 -0700

    sum2_digit.cpp

---
+++ sum2_digit.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,sum;
+	clrscr();
+	cout<<"enter a two digit=";
+	cin>>n1>>n2;
+	sum=n1+n2;
+	cout<<"sum="<<sum<<endl;
+	getch();
+	return 0;
+
+}
